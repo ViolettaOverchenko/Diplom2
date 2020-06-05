1b938acdb2e718475055f7953e300e7b2c8c13f2
Violetta <owerchenko.violetta@yandex.ru>
2020-06-05 12:22:22 +0700

    js

---
+++ mysite2/static/analysis_material/js/analysis_material.js
@@ -0,0 +1,23 @@
+function first() {
+document.getElementById("second_hide").setAttribute("style", "opacity:1; transition: 1s; height: 100%;");
+document.getElementById("first").setAttribute("style", "display: none");
+document.getElementById("first_yelloy").setAttribute("style", "display: block");
+}
+
+function first_yelloy() {
+document.getElementById("second_hide").setAttribute("style", "display: none");
+document.getElementById("first_yelloy").setAttribute("style", "display: none");
+document.getElementById("first").setAttribute("style", "display: block");
+}
+
+function first2() {
+document.getElementById("second_hide2").setAttribute("style", "opacity:1; transition: 1s; height: 100%;");
+document.getElementById("first2").setAttribute("style", "display: none");
+document.getElementById("first_yelloy2").setAttribute("style", "display: block");
+}
+
+function first_yelloy2() {
+document.getElementById("second_hide2").setAttribute("style", "display: none");
+document.getElementById("first_yelloy2").setAttribute("style", "display: none");
+document.getElementById("first2").setAttribute("style", "display: block");
+}
